8df8aa235b80909f69f4475f7f6db8fb30334fbc
realeigenvalue <57877219+realeigenvalue@users.noreply.github.com>
2019-12-03 04:24:21 -0600

    Add Main class

---
+++ src/main/java/com/realeigenvalue/jchess/Main.java
@@ -0,0 +1,9 @@
+package com.realeigenvalue.jchess;
+
+import com.realeigenvalue.jchess.mvc.ChessApp;
+
+public class Main {
+	public static void main(String[] args) {
+		ChessApp chessApp = new ChessApp();
+	}
+}
